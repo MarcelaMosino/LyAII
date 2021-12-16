451e46c8dacf3c588d98c5848c2350fed6ae44fd
Marcela Mosiño <68758494+MarcelaMosino@users.noreply.github.com>
2021-12-16 13:06:00 -0600

    Delete LyA directory

--- LyA/src/cola/Encolable.java
+++
@@ -1,8 +0,0 @@
-
-package cola;
-
-public interface Encolable<T> {
-    public T dequeue();
-    public void enqueue(T elemento);
-    public boolean isEmpty();
-}
--- LyA/src/pila/Apilable.java
+++
@@ -1,9 +0,0 @@
-
-package pila;
-
-public interface Apilable <T>{
-    public T pop();
-    public T peek();
-    public void push(T elemento);
-    public boolean isEmpty();
-}
